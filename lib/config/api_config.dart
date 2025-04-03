217d355183aa43cb5450c80f5ddfbb08beb14c60
Lucas Bernardeau <lucas.bernardeau@epita.fr>
2025-04-03 18:19:26 +0200

    article

---
+++ lib/config/api_config.dart
@@ -0,0 +1,6 @@
+// lib/config/api_config.dart
+// IMPORTANT : Ce fichier NE DOIT PAS ÊTRE versionné (ajouté à Git).
+// Ajoutez 'lib/config/api_config.dart' à votre fichier .gitignore !
+
+// Remplacez la chaîne ci-dessous par VOTRE VRAIE clé API NYT
+const String nytApiKey = "cApjauDgzNOmv6j5I08Q93V2SoNmRCxg";
